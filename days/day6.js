cad4a043c10aebfec441a7e1ab67a3b5028b1062
Nadia Mitterer <nadia.mitterer@yahoo.it>
2022-12-06 08:56:50 +0100

    reworked day 6

--- days/day6.js
+++ days/day6.js
@@ -3,44 +3,15 @@ import Helper from '../helpers.js'
 const currentDay = process.argv.slice(1)[0].split('\\').pop().split('.')[0] + ".txt"
 const input = Helper.textToString(process.argv.length > 2 ? process.argv[2] : currentDay)
 
-let result1 = 0
-let result2 = 0
-const groupOfFour = []
-const groupOf14 = []
 
-function allDifferent(array){
-    for(let i = 0; i < array.length; i++){
-        if(array.includes(array[i], i+1)){
-            return false
+function findFirstMarker(string, size){
+    for(let i = 0; i < string.length; i++){
+        if(new Set(string.substring(i, i+size)).size === size){
+            return i+size
         }
     }
-    return true
+    return 0
 }
 
-for(let i = 0; i < input.length; i++){
-    if(!result1){
-        groupOfFour.push(input[i])
-        if(groupOfFour.length === 4){
-            if(allDifferent(groupOfFour)){
-                result1 = i+1
-            }
-            else{
-                groupOfFour.shift()
-            }
-        }
-    }
-    if(!result2){
-        groupOf14.push(input[i])
-        if(groupOf14.length === 14){
-            if(allDifferent(groupOf14)){
-                result2 =  i+1
-            }
-            else{
-                groupOf14.shift()
-            }
-        }
-    }
-}
-
-console.log("part 1:", result1)
-console.log("part 2:", result2)
+console.log("part 1:", findFirstMarker(input, 4))
+console.log("part 2:", findFirstMarker(input, 14))
