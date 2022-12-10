d9e0a629d07827d8985a49f37af3d32c4b1613fd
Nadia Mitterer <nadia.mitterer@yahoo.it>
2022-12-10 12:03:21 +0100

    finished day 10

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-![](https://img.shields.io/badge/stars%20⭐-18-yellow)
-![](https://img.shields.io/badge/days%20completed%20📅-9-blue)
+![](https://img.shields.io/badge/stars%20⭐-20-yellow)
+![](https://img.shields.io/badge/days%20completed%20📅-10-blue)
 
 # AdventOfCode2022
 
---
+++ days/day10.js
@@ -0,0 +1,73 @@
+import Helper from '../helpers.js'
+
+const currentDay = process.argv.slice(1)[0].split('\\').pop().split('.')[0] + ".txt"
+const input = Helper.textToStringArray(process.argv.length > 2 ? process.argv[2] : currentDay)
+
+function part1(){
+    let registerX = 1
+    let result = 0
+    const cycles = []
+
+    for(const line of input){
+        cycles.push(0)
+
+        if(line !== "noop"){
+            cycles.push(parseInt(line.split(" ")[1]))
+        }
+    }
+
+
+    for(let cycle = 1; cycles.length>0; cycle++){
+        if((cycle-20)%40 === 0){
+            result += registerX*cycle
+        }
+
+        registerX += cycles.shift()
+    }
+    
+    return result
+}
+
+function printPixels(pixels){
+    for(let y = 0; y < pixels.length; y++){
+        let line = ""
+        for(let x = 0; x < pixels[y].length; x++){
+            line += pixels[y][x] ? "\u2588" : " "
+        }
+        console.log(line)
+    }
+}
+
+Number.prototype.between = function(min, max) {
+    return this >= min && this <= max
+}
+
+function part2(){
+    let registerX = 1
+    const pixels = Array.from({length:6}, _ => {return Array.from({length:40}, _ => {return false})})
+    const cycles = []
+
+    for(const line of input){
+        cycles.push(0)
+
+        if(line !== "noop"){
+            cycles.push(parseInt(line.split(" ")[1]))
+        }
+    }
+
+    for(let ctrPos = 0; cycles.length>0; ctrPos++){
+        if((ctrPos%40).between(registerX-1, registerX+1)){
+            pixels[parseInt(ctrPos/40)][parseInt(ctrPos%40)] = true
+        }
+
+        registerX += cycles.shift()
+    }
+    
+    printPixels(pixels)
+}
+
+
+
+console.log("part 1:", part1())
+console.log("part 2:")
+part2()
